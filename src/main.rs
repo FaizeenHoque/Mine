5b95285176d74cabbc413a6cb78d6000e1e464e9
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-07-23 12:32:34 +0600

    Implemented Mesh struct

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,70 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::video::GLProfile;
 
+struct Mesh {
+    vao: glow::VertexArray,
+    vbo: glow::Buffer,
+    ebo: glow::Buffer,
+    index_count: i32,
+}
+
+impl Mesh {
+    pub fn new(gl: &glow::Context, vertices: &[f32], indices: &[u32]) -> Result<Self, String> {
+        unsafe {
+            let vao = gl.create_vertex_array()?;
+            let vbo = gl.create_buffer()?;
+            let ebo = gl.create_buffer()?;
+
+            gl.bind_vertex_array(Some(vao));
+
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+            gl.buffer_data_u8_slice(
+                glow::ARRAY_BUFFER,
+                bytemuck::cast_slice(vertices),
+                glow::STATIC_DRAW,
+            );
+
+            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
+            gl.buffer_data_u8_slice(
+                glow::ELEMENT_ARRAY_BUFFER,
+                bytemuck::cast_slice(indices),
+                glow::STATIC_DRAW,
+            );
+
+            let stride = 6 * std::mem::size_of::<f32>() as i32;
+
+            // position
+            gl.enable_vertex_attrib_array(0);
+            gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, stride, 0);
+
+            // color
+            gl.enable_vertex_attrib_array(1);
+            gl.vertex_attrib_pointer_f32(
+                1,
+                3,
+                glow::FLOAT,
+                false,
+                stride,
+                3 * std::mem::size_of::<f32>() as i32,
+            );
+
+            Ok(Self {
+                vao,
+                vbo,
+                ebo,
+                index_count: indices.len() as i32,
+            })
+        }
+    }
+
+    pub fn draw(&self, gl: &glow::Context) {
+        unsafe {
+            gl.bind_vertex_array(Some(self.vao));
+            gl.draw_elements(glow::TRIANGLES, self.index_count, glow::UNSIGNED_INT, 0);
+        }
+    }
+}
+
 fn main() -> Result<(), String> {
     let sdl = sdl2::init()?;
     let video = sdl.video()?;
@@ -67,50 +131,8 @@ fn main() -> Result<(), String> {
 
     let offset_location = unsafe { gl.get_uniform_location(shader_program, "offset") };
 
-    let vao = unsafe { gl.create_vertex_array()? };
-    unsafe {
-        gl.bind_vertex_array(Some(vao));
-    }
-    let vbo = unsafe { gl.create_buffer()? };
-    unsafe {
-        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-    };
-
-    let ebo = unsafe { gl.create_buffer()? };
-    unsafe {
-        gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
-    };
-
-    unsafe {
-        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-    }
-    unsafe {
-        gl.enable_vertex_attrib_array(0);
-    }
-
     let mut x = 0.0f32;
     let mut y = 0.0f32;
-
-    unsafe {
-        let stride = 6 * std::mem::size_of::<f32>() as i32;
-
-        // position
-        gl.enable_vertex_attrib_array(0);
-        gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, stride, 0);
-
-        // color
-        gl.enable_vertex_attrib_array(1);
-        gl.vertex_attrib_pointer_f32(
-            1,
-            3,
-            glow::FLOAT,
-            false,
-            stride,
-            3 * std::mem::size_of::<f32>() as i32,
-        );
-
-        gl.uniform_2_f32(offset_location.as_ref(), x, y);
-    }
 
     let vertices: [f32; 24] = [
         0.5, 0.5, 0.0, 1.0, 0.0, 0.0, -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, -0.5, -0.5, 0.0, 0.0, 0.0,
@@ -119,19 +141,7 @@ fn main() -> Result<(), String> {
 
     let indices: [u32; 6] = [0, 1, 2, 0, 3, 2];
 
-    unsafe {
-        gl.buffer_data_u8_slice(
-            glow::ARRAY_BUFFER,
-            bytemuck::cast_slice(&vertices),
-            glow::STATIC_DRAW,
-        );
-
-        gl.buffer_data_u8_slice(
-            glow::ELEMENT_ARRAY_BUFFER,
-            bytemuck::cast_slice(&indices),
-            glow::STATIC_DRAW,
-        );
-    }
+    let mesh = Mesh::new(&gl, &vertices, &indices)?;
 
     let mut event_pump = sdl.event_pump()?;
 
@@ -172,17 +182,12 @@ fn main() -> Result<(), String> {
         unsafe {
             gl.clear_color(0.1, 0.2, 0.3, 1.0);
             gl.clear(glow::COLOR_BUFFER_BIT);
-        }
 
-        unsafe {
             gl.use_program(Some(shader_program));
-
             gl.uniform_2_f32(offset_location.as_ref(), x, y);
-
-            gl.bind_vertex_array(Some(vao));
-
-            gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
         }
+
+        mesh.draw(&gl);
 
         window.gl_swap_window();
     }
